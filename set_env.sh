57f20f39580f9d9733d19e1d3e525e551974cc05
gang.xu <gang.xu@iluvatar.ai>
2019-10-25 17:04:37 +0800

    Add set_env shell script and update README.md

---
+++ set_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GPU_ID=2 #Change this to your GPU id
+MAX_SM_CLOCK=`nvidia-smi -q -d CLOCK -i ${GPU_ID} | grep -A4 "Max Clocks" | grep SM | awk '{print $3}'`
+MAX_MEM_CLOCK=`nvidia-smi -q -d CLOCK -i ${GPU_ID} | grep -A4 "Max Clocks" | grep Memory | awk '{print $3}'`
+nvidia-smi -pm 1 -i ${GPU_ID}
+nvidia-smi -ac ${MAX_MEM_CLOCK},${MAX_SM_CLOCK} -i ${GPU_ID}
